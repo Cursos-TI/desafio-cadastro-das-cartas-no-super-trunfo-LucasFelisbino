cbb8d7e46b5523d6fa92417336e5a993313b31fc
LucasFelisbino <lucas.felisbno11@gmail.com>
2025-03-14 17:00:41 +0000

    carta1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    printf ("Desafio Super Trunfo");\
-        char Estado1, Código1[8], Nome1[50];
-        int população1, Pontos_turisticos1;
-        float Área1, PIB1;
-        char Estado2, Código2[8], Nome2[50];
-        int população2, Pontos_turisticos2;
-        float Área2, PIB2;
-    printf ("Digite o nome do Estado: \n");
-    scanf ("%s", &Estado1);
+    printf ("Desafio Super Trunfo \n");
+        char Estado1 = 'SP';
+        int Codigo1[8] = 01; 
+        char Nome1[50] = "Sãopaulo" ;
+        int população1 = 11.45, Pontos_turisticos1 = 15;
+        float Área1 = 1.521, PIB1 = 2.719;
+    printf ("Estado: %s Estado1 \n", Estado1);
+
+    printf ("Codigo da Carta: %d &Código1 \n", Codigo1);
     
-    printf ("Digite o Codigo da Carta: \n");
-    scanf ("%s", &Código1);
+    printf ("Nome da cidade: %s &Nome1 \n", Nome1);
     
-    printf ("Digite o nome da cidade: \n");
-    scanf ("%s", &Nome1);
+    printf ("População: %d &população1\n", população1);
     
-    printf ("Digite a População: \n");
-    scanf ("%d", &população1);
+    printf ("Área em km²: %f &Área1) \n", Área1);
     
-    printf ("Digite a Área em km²: \n");
-    scanf ("%f", &Área1);
-    
-    printf ("Digite o PIB: \n");
-    scanf ("%f", &PIB1);
-    
-    printf ("Digite o numero de pontos turisticos: \n");
-    scanf ("%d", &Pontos_turisticos1);
+    printf ("PIB: %f &PIB1 \n", PIB1);
+ 
+    printf ("Numero de pontos turisticos: %d &Pontos_turisticos1 \n", Pontos_turisticos1);
+  
 
    return 0;
 }
