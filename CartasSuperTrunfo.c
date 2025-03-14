5f60e3c40ec767910c364bdc4baa480a7fca96fc
LucasFelisbino <lucas.felisbno11@gmail.com>
2025-03-14 17:13:10 +0000

    carta1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    printf ("Desafio Super Trunfo \n");
+    printf ("Desafio Super Trunfo\n");
         char Estado1 = 'SP';
         int Codigo1[8] = 01; 
         char Nome1[50] = "Sãopaulo" ;
         int população1 = 11.45, Pontos_turisticos1 = 15;
         float Área1 = 1.521, PIB1 = 2.719;
-    printf ("Estado: %s Estado1 \n", Estado1);
+    printf ("Estado: %s\n", Estado1);
 
-    printf ("Codigo da Carta: %d &Código1 \n", Codigo1);
+    printf ("Codigo da Carta: %d\n", Codigo1);
     
-    printf ("Nome da cidade: %s &Nome1 \n", Nome1);
+    printf ("Nome da cidade: %s\n", Nome1);
     
-    printf ("População: %d &população1\n", população1);
+    printf ("População: %d\n", população1);
     
-    printf ("Área em km²: %f &Área1) \n", Área1);
+    printf ("Área em km²: %f\n", Área1);
     
-    printf ("PIB: %f &PIB1 \n", PIB1);
+    printf ("PIB: %f\n", PIB1);
  
-    printf ("Numero de pontos turisticos: %d &Pontos_turisticos1 \n", Pontos_turisticos1);
-  
-
-   return 0;
+    printf ("Numero de pontos turisticos: %d\n", Pontos_turisticos1);
+    
+    return 0;
 }
