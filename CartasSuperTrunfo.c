9696ff69d72a8c3c4332b1d44969d15d7e532a64
LucasFelisbino <lucas.felisbno11@gmail.com>
2025-03-17 17:56:56 +0000

    Cartas1&2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,25 +1,52 @@
 #include <stdio.h>
 
-int main() {
-    printf ("Desafio Super Trunfo\n");
-        char Estado1 = 'SP';
-        int Codigo1[8] = 01; 
-        char Nome1[50] = "Sãopaulo" ;
-        int população1 = 11.45, Pontos_turisticos1 = 15;
-        float Área1 = 1.521, PIB1 = 2.719;
-    printf ("Estado: %s\n", Estado1);
+int main (){
+    char estado1 = 'A';
+    char codigo1[8] = "A01";
+    char nome1[10] = "SaoPaulo";
+    int populacao1 = 12325000;
+    int Pontos_Turisticos1 = 50;
+    float area1 = 1521.11;
+    float pib1 = 699.28;
 
-    printf ("Codigo da Carta: %d\n", Codigo1);
-    
-    printf ("Nome da cidade: %s\n", Nome1);
-    
-    printf ("População: %d\n", população1);
-    
-    printf ("Área em km²: %f\n", Área1);
-    
-    printf ("PIB: %f\n", PIB1);
- 
-    printf ("Numero de pontos turisticos: %d\n", Pontos_turisticos1);
-    
+    printf ("o Estado e: %c\n", estado1);
+
+    printf ("O codigo e: %s\n", codigo1);
+
+    printf ("O nome e: %s\n", nome1);
+
+    printf ("A Populacao e de: %d\n", populacao1);
+
+    printf ("a Quantidade de Pontos turiscos e de:%d\n", Pontos_Turisticos1);
+
+    printf ("A areae e de: %f\n", area1);
+
+    printf ("O Pib e de: %f\n", pib1);
+
+    printf ("Carta2\n");
+
+    char estado2 = 'B';
+    char codigo2[8] = "B02";
+    char nome2[14] = "RioDeJaneiro";
+    int populacao2 = 6748000;
+    int Pontos_Turisticos2 = 30;
+    float area2 = 1200.25;
+    float pib2 = 300.50;
+
+    printf ("o Estado e: %c\n", estado2);
+
+    printf ("O codigo e: %s\n", codigo2);
+
+    printf ("O nome e: %s\n", nome2);
+
+    printf ("A Populacao e de: %d\n", populacao2);
+
+    printf ("a Quantidade de Pontos turiscos e de:%d\n", Pontos_Turisticos2);
+
+    printf ("A areae e de: %f\n", area2);
+
+    printf ("O Pib e de: %f\n", pib2);
+
     return 0;
-}
+    
+
